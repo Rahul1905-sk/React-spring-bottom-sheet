e4d04d18d0ff64ba3db99cb84c2764c7ea172bf1
Rahul Singh <rahul1905.sk@gmail.com>
2023-09-16 11:48:49 +0530

    final update

--- src/components/ModalContent.jsx
+++ src/components/ModalContent.jsx
@@ -1,62 +1,29 @@
-import React, { useEffect, useState } from "react";
-
+ 
 const ModalContent = () => {
   return (
+    
     <div className="modal-container">
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi velit
-      architecto impedit voluptatibus! Minus accusantium officiis distinctio
-      quibusdam quas atque?
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi velit
-      architecto impedit voluptatibus! Minus accusantium officiis distinctio
-      quibusdam quas atque?
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi velit
-      architecto impedit voluptatibus! Minus accusantium officiis distinctio
-      quibusdam quas atque?
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi velit
-      architecto impedit voluptatibus! Minus accusantium officiis distinctio
-      quibusdam quas atque?
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi velit
-      architecto impedit voluptatibus! Minus accusantium officiis distinctio
-      quibusdam quas atque?
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi velit
-      architecto impedit voluptatibus! Minus accusantium officiis distinctio
-      quibusdam quas atque?
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi velit
-      architecto impedit voluptatibus! Minus accusantium officiis distinctio
-      quibusdam quas atque?
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi velit
-      architecto impedit voluptatibus! Minus accusantium officiis distinctio
-      quibusdam quas atque?
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi velit
-      architecto impedit voluptatibus! Minus accusantium officiis distinctio
-      quibusdam quas atque?
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi velit
-      architecto impedit voluptatibus! Minus accusantium officiis distinctio
-      quibusdam quas atque?
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi velit
-      architecto impedit voluptatibus! Minus accusantium officiis distinctio
-      quibusdam quas atque?
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi velit
-      architecto impedit voluptatibus! Minus accusantium officiis distinctio
-      quibusdam quas atque?
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi velit
-      architecto impedit voluptatibus! Minus accusantium officiis distinctio
-      quibusdam quas atque?
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi velit
-      architecto impedit voluptatibus! Minus accusantium officiis distinctio
-      quibusdam quas atque?
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi velit
-      architecto impedit voluptatibus! Minus accusantium officiis distinctio
-      quibusdam quas atque?
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi velit
-      architecto impedit voluptatibus! Minus accusantium officiis distinctio
-      quibusdam quas atque?
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi velit
-      architecto impedit voluptatibus! Minus accusantium officiis distinctio
-      quibusdam quas atque?
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi velit
-      architecto impedit voluptatibus! Minus accusantium officiis distinctio
-      quibusdam quas atque?
+<h1 >5 Apps that simplified AR Content Creation for Social Media</h1>
+    <p style={{margin:"10px", fontSize:'18px'}}> <br /> <span className="bold">1. Flam</span>  <br />
+Flam is at the forefront of AR creation in India, with its app spearheading the movement towards implementing Augmented Reality for social media. The Flam App allows you to create your 3D Avatar with the choice of stylised or photo-realistic avatar, and once your preferred avatar is ready, you can create using the Augmented Reality tech in the app to create 3D AR content right from your phone in real-time!
+
+You can dance, act or do anything with your 3D avatar in your space and then upload the video to social media that stand apart from the traditional!
+
+ <br /> <br /> <span className="bold">2. Sensorium</span>  <br />
+The next app on the list is Sensorium which allows you to create AR content with one of your selected avatars. For starters, you can assign your created avatar a personality from the list of personalities and chat with him/her. The avatar will replies are based on the personality you choose. You can also make the avatar dance in AR or a 3D environment with a personal mix of the preset moves on the app.
+
+<br /> <br /> <span className="bold">3. Meteora</span>  <br />
+Meteora’s AR creation also has similar features, as in you can create your avatar and perform dances with your avatar. The app also has Motion Capture, which allows you to record your moves and makes your avatar perform the same moves.
+
+Other than that, the features are geared towards user engagement rather than creating AR content. Things like Fashion Battle and Feed are present to make the user keep returning to the app.
+
+  <br /> <br /> <span className="bold">4. Zepeto </span>  <br />
+Fourth on this list is Zepeto, a social metaverse app that allows you to hang out in their virtual space with other users. Along with features like cartoon-styled avatar creation, there is a templated content creation format that users can use along with their AR mode.
+
+There is little that you can do on the AR front on the app as it is a more 3D environment focused app, but users can get a taste of AR content creation with this app.
+
+ <br /> <br /> <span className="bold">5. Jadu AR  </span>  <br />  
+Jadu AR is an invite-only app (at the time of writing this article) focused on more AR experience with their 3D avatars. The type of activities on the app, like avatar PvP fighting, tend to cater to individual preferences rather than actual ‘content creation’. You can still create niche and nuanced content on the app for your social media, but it could be a little tricky.</p>
     </div>
   );
 };
